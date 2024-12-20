73554cf47ff40226bfd7a5c5cab0ec3ead758689
Jakob Haller <haller.jakob@outlook.de>
2024-12-20 13:27:51 +0100

    Brainstorming

--- src/main.js
+++ src/main.js
@@ -7,18 +7,36 @@ import { isInside } from './ray-casting.js';
 function main() {
     const margin = 50;
     const polygon = new Boundary([
-        { x: 0 + margin, y: 0 + margin },
-        { x: 50 + margin, y: 0 + margin },
-        { x: 50 + margin, y: 50 + margin },
-        { x: 25 + margin, y: 25 + margin },
-        { x: 0 + margin, y: 50 + margin },
+        { x: 100, y: 100 },
+        { x: 400, y: 100 },
+        { x: 400, y: 400 },
+        { x: 300, y: 300 },
+        { x: 200, y: 300 },
+        { x: 200, y: 400 },
+        { x: 100, y: 400 },
     ]);
     polygon.draw();
 
-    const point = new Vector(75, 50);
-    point.draw();
-
-    console.log(isInside(polygon.vertices, point));
+    const points = [
+        new Vector(400, 400),
+        new Vector(400, 200),
+        new Vector(400, 100),
+        new Vector(300, 400),
+        new Vector(300, 300),
+        new Vector(300, 200),
+        new Vector(300, 100),
+        new Vector(300, 0),
+        new Vector(100, 200),
+        new Vector(100, 400),
+        new Vector(100, 100),
+        new Vector(0, 400),
+        new Vector(0, 300),
+        new Vector(0, 200),
+        new Vector(0, 100),
+    ];
+    for (const point of points) {
+        point.draw();
+    }
 }
 
 window.addEventListener('load', main);
